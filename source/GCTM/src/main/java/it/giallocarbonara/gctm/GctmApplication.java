5f7eee50676d8bd7cc9a0a070786ec9d274f8d02
Pietro Francesco Libri <pietrofrancesco.libri@gmail.com>
2026-03-06 11:13:59 +0100

    Added Spring Maven Project

---
+++ source/GCTM/src/main/java/it/giallocarbonara/gctm/GctmApplication.java
@@ -0,0 +1,13 @@
+package it.giallocarbonara.gctm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GctmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GctmApplication.class, args);
+    }
+
+}
---
+++ source/GCTM/src/test/java/it/giallocarbonara/gctm/GctmApplicationTests.java
@@ -0,0 +1,13 @@
+package it.giallocarbonara.gctm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GctmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
