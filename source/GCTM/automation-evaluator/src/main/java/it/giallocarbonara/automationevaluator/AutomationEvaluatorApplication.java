e0910a1d1c0cf718d790a6f1a772bb1524436c8a
Pietro Francesco Libri <pietrofrancesco.libri@gmail.com>
2026-03-10 05:22:39 +0100

    Draft of Logger with ElasticSearch (not working)

--- source/GCTM/automation-evaluator/src/main/java/it/giallocarbonara/automationevaluator/AutomationEvaluatorApplication.java
+++ source/GCTM/automation-evaluator/src/main/java/it/giallocarbonara/automationevaluator/AutomationEvaluatorApplication.java
@@ -1,5 +1,6 @@
 package it.giallocarbonara.automationevaluator;
 
+import it.giallocarbonara.SensorData;
 import it.giallocarbonara.UnifiedEnvelope;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +12,10 @@ public class AutomationEvaluatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutomationEvaluatorApplication.class, args);
     }
-    @JmsListener(destination = "mars.telemetry.topic")
-    public void onMessage(UnifiedEnvelope message) {
-        System.out.println("📥 [RECEIVER] SUCCESS! Received data from: " + message.header().origin());
-        System.out.println("📊 Metric: " + message.payload().metrics().get(0).name() +
-                " = " + message.payload().metrics().get(0).value());
+    @JmsListener(destination = "sensors.topic")
+    public void onMessage(SensorData sensorData) {
+        System.out.println("📥 [RECEIVER] SUCCESS! Received data from: " + sensorData.header().sender());
+        System.out.println("📊 Metric: " + sensorData.metrics().getFirst().name() +
+                " = " + sensorData.metrics().getFirst().value());
     }
 }
