f07f504dd3178ad007a475f3b90a86c30c7ce709
Pietro Francesco Libri <pietrofrancesco.libri@gmail.com>
2026-03-10 17:52:34 +0100

    Cleaned the branch from test data in other services

--- source/GCTM/automation-evaluator/src/main/java/it/giallocarbonara/automationevaluator/AutomationEvaluatorApplication.java
+++ source/GCTM/automation-evaluator/src/main/java/it/giallocarbonara/automationevaluator/AutomationEvaluatorApplication.java
@@ -1,13 +1,8 @@
 package it.giallocarbonara.automationevaluator;
 
-import it.giallocarbonara.UnifiedEnvelope;
-import it.giallocarbonara.automationevaluator.entity.AutomationRule;
-import it.giallocarbonara.automationevaluator.repository.RuleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.annotation.JmsListener;
+
 
 @SpringBootApplication(scanBasePackages = "it.giallocarbonara")
 public class AutomationEvaluatorApplication {
--- source/GCTM/automation-evaluator/src/main/java/it/giallocarbonara/automationevaluator/repository/RuleRepository.java
+++
@@ -1,9 +0,0 @@
-package it.giallocarbonara.automationevaluator.repository;
-
-import it.giallocarbonara.automationevaluator.entity.AutomationRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RuleRepository extends JpaRepository<AutomationRule, Long> {
-    List<AutomationRule> findBySensorNameIgnoreCase(String metricName);
-}
